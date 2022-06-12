61b0994ac67bdb5626748a9d75f11cd2e5233dd2
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-06-13 00:51:16 +0200

    switched to OpenStreetMap

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -12,8 +12,8 @@ const MapComponent = ({ origin, destination }: RouteProps) => {
       center={[33.5024, 36.2988]}
     >
       <TileLayer
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-        attribution="Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       {origin && destination &&
         <RoutingMachine
